53bae0461b2d45a114022cc926ba83a0e42f6376
cath-app <dumagtoycatherine29@gmail.com>
2024-11-26 15:08:41 +0800

    Update cath.py

--- cath.py
+++ cath.py
@@ -1,42 +1,49 @@
 import streamlit as st
 
-st.title("Student Biography: [Catherine T. Dumagtoy]")
+# Title
+st.title("Student Biography: Catherine T. Dumagtoy")
 
+# About Me
 st.header("About Me")
 st.image("cath.jpg", caption="This is me!", use_column_width=True)
 st.write("""
-    Hello! I'm **[Catherine]**, a student pursuing a degree in [Bachelor of Science in Computer Engineering] at [Surigao Del Norte State University]. 
+    Hello! I'm **Catherine**, a student pursuing a degree in **Bachelor of Science in Computer Engineering** at **Surigao Del Norte State University**. 
     On this blog, I'll share a little bit about my background, academic interests, projects, and more.
 """)
 
+# Personal Details
 st.header("Personal Details")
 st.write("""
-    - **Full Name**: [Catherine T. Dumagtoy]
-    - **Date of Birth**: [July 2, 2006)]
-    - **Gender**: [Female]
-    - **Location**: [Surigao City, Philippines]
-    - **Nationality**: [Filipino]
+    - **Full Name**: Catherine T. Dumagtoy
+    - **Date of Birth**: July 2, 2006
+    - **Gender**: Female
+    - **Location**: Surigao City, Philippines
+    - **Nationality**: Filipino
+""")
 
+# Educational Attainment
 st.header("Educational Attainment")
 st.subheader("Elementary School")
 st.write("""
-    - **School Name**: [Surigao West Central Elementary School]
-    - **Duration**: [2013] - [2018]
+    - **School Name**: Surigao West Central Elementary School
+    - **Duration**: 2013 - 2018
 """)
 st.subheader("Middle School")
 st.write("""
-    - **School Name**: [Surigao City National High School]
-    - **Duration**: [2018] - [2024]
-""")
-         
-st.header("My Interests")
-st.write("""
-    In addition to my professional work, I enjoy [Reading and Listening to music]. 
-    Some of my favorite activities include:
-    - [Watching movies]
-    - [Playing video games]
-    - [Sleeping]
+    - **School Name**: Surigao City National High School
+    - **Duration**: 2018 - 2024
 """)
 
+# My Interests
+st.header("My Interests")
+st.write("""
+    In addition to my professional work, I enjoy **Reading** and **Listening to music**. 
+    Some of my favorite activities include:
+    - Watching movies
+    - Playing video games
+    - Sleeping
+""")
+
+# Contact Me
 st.header("Contact Me")
 st.markdown("[Email Me](mailto:your-dumagtoycatherine29@gmail.com)")
