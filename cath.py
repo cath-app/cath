829519d267bb9f027c6551758dc719ade887aa71
cath-app <dumagtoycatherine29@gmail.com>
2024-11-26 15:12:07 +0800

    Update cath.py

--- cath.py
+++ cath.py
@@ -3,7 +3,6 @@ import streamlit as st
 # Title
 st.title("Student Biography: Catherine T. Dumagtoy")
 
-# About Me
 st.header("About Me")
 st.image("cath.jpg", caption="This is me!", use_column_width=True)
 st.write("""
@@ -11,7 +10,6 @@ st.write("""
     On this blog, I'll share a little bit about my background, academic interests, projects, and more.
 """)
 
-# Personal Details
 st.header("Personal Details")
 st.write("""
     - **Full Name**: Catherine T. Dumagtoy
@@ -28,13 +26,12 @@ st.write("""
     - **School Name**: Surigao West Central Elementary School
     - **Duration**: 2013 - 2018
 """)
-st.subheader("Middle School")
+st.subheader("High Scool")
 st.write("""
     - **School Name**: Surigao City National High School
     - **Duration**: 2018 - 2024
 """)
 
-# My Interests
 st.header("My Interests")
 st.write("""
     In addition to my professional work, I enjoy **Reading** and **Listening to music**. 
@@ -44,6 +41,5 @@ st.write("""
     - Sleeping
 """)
 
-# Contact Me
 st.header("Contact Me")
 st.markdown("[Email Me](mailto:your-dumagtoycatherine29@gmail.com)")
